fb03337f40ed750cd36060d3122ec65c76df6f39
KrizTech <christiandonatus01@gmail.com>
2022-12-01 16:29:45 -0800

    Updared Task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
 
-    print(a)
+    from sys import argv, exit
+
+    if (len(argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if not (argv[2] in '+-*/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    from calculator_1 import add, sub, mul, div
+
+    z = {
+        "x": int(argv[1]),
+        "y":  int(argv[3]),
+        "opr": argv[2],
+        "res": 0
+    }
+
+    if z['opr'] == '+':
+        z['res'] = add(z['x'], z['y'])
+
