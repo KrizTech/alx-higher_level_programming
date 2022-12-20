85aaf1618cc8ac6813c4b037082009b6f2510a8b
KrizTech <christiandonatus01@gmail.com>
2022-12-20 14:38:30 -0800

    Updated commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-''' A Module that creates a Square object '''
+"""defines a class Square"""
 
-class Square;
-''' Creating an Object template '''
-    def __init__(self, size = 0):
-        '''
-            The init method initializes the class instance
 
-        @self:
-            A parameter used to refer to the class instance
-
-        @size:
-            The size of the square, must be a +ve integer
-        '''
-        if type(size) is int:
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
             if size < 0:
-                raise ValueError('Size must be >= 0')
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size
-        else:
-            raise TypeError('Size must be an integer')
