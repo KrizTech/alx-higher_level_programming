4eee95617aa4e02f174103807bb84626b64917d0
KrizTech <christiandonatus01@gmail.com>
2023-04-02 15:59:57 -0700

    Network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-Script that fetches https://intranet.hbtn.io/status
-"""
-import urllib.request
 
 
-def main():
-    """
-    Funtion to print a response of a specific url
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__main__":
-    main()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
